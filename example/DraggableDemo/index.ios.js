89f40837ca69e40b535be704a8ee01413bad22f3
tony <tony@xxx.com>
2017-05-11 17:47:45 +0800

    feature

--- example/DraggableDemo/index.ios.js
+++ example/DraggableDemo/index.ios.js
@@ -8,94 +8,48 @@ import React, { Component } from 'react';
 import {
 	AppRegistry,
 	StyleSheet,
-	View,
 	Text,
-	PanResponder,
-	Animated,
-	Dimensions,
-	Button,
-	TouchableOpacity
+	View
 } from 'react-native';
-
+import Draggable from 'react-native-draggable';
 
 export default class DraggableDemo extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			pan : new Animated.ValueXY()	 
-		};
-		this.panResponder = PanResponder.create({		
-			onMoveShouldSetPanResponder : (evt, gestureState) => true,
-			onMoveShouldSetPanResponderCapture : (evt, gestureState) => true,
-			onPanResponderMove : Animated.event([null,{ 
-				dx : this.state.pan.x,
-				dy : this.state.pan.y
-			}]),
-			onPanResponderRelease : (e, gesture) => {
-				Animated.spring(						
-					this.state.pan,				 
-					{toValue:{x:0,y:0}}		 
-				).start();
-			} 
-		});
-	}
-	renderDraggable() {
-		return (
-			<View style={styles.draggableContainer}>
-				<Animated.View 
-					{...this.panResponder.panHandlers}
-					style={[this.state.pan.getLayout(), styles.circle]}>
-					<TouchableOpacity 
-						style={styles.circle}
-						onPress={()=> alert('123')}>
-					</TouchableOpacity>
-					
-				</Animated.View>
-			</View>
-		);
-	}
 	render() {
 		return (
-			<View style={styles.mainContainer}>
-				<View style={styles.dropZone}>
-						<Text style={styles.text}>Drop me here!</Text>
-				</View>
-
-				{this.renderDraggable()}
+			<View style={styles.container}>
+				<Draggable pressDrag={() => alert('touched')}/> 
+				<Text style={styles.welcome}>
+					Welcome to React Native!
+				</Text>
+				<Text style={styles.instructions}>
+					To get started, edit index.android.js
+				</Text>
+				<Text style={styles.instructions}>
+					Double tap R on your keyboard to reload,{'\n'}
+					Shake or press menu button for dev menu
+				</Text>
 			</View>
 		);
 	}
 }
 
-let CIRCLE_RADIUS = 36;
-let Window = Dimensions.get('window');
-let styles = StyleSheet.create({
-	mainContainer : {
-		flex : 1
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
 	},
-	dropZone : {
-		height : 100,
-		backgroundColor : '#2c3e50'
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
 	},
-	text : {
-		marginTop : 25,
-		marginLeft : 5,
-		marginRight : 5,
-		textAlign : 'center',
-		color : '#fff'
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
 	},
-	draggableContainer: {
-		position : 'absolute',
-		top : Window.height/2 - CIRCLE_RADIUS,
-		left : Window.width/2 - CIRCLE_RADIUS,
-	},
-	circle : {
-		backgroundColor	: '#1abc9c',
-		width : CIRCLE_RADIUS*2,
-		height : CIRCLE_RADIUS*2,
-		borderRadius : CIRCLE_RADIUS
-	}
 });
 
 AppRegistry.registerComponent('DraggableDemo', () => DraggableDemo);
