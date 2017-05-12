ede1ba744efcb8eef0b61d8da4dfcf47c7b90e4e
Tony ZL Yang <tony0x00@gmail.com>
2017-05-13 00:02:08 +0800

    add reverse animate feature

--- Draggable.js
+++ Draggable.js
@@ -11,7 +11,6 @@ import {
 	PanResponder,
 	Animated,
 	Dimensions,
-	Button,
 	TouchableOpacity
 } from 'react-native';
 
@@ -21,48 +20,87 @@ export default class Draggable extends Component {
 		renderText:React.PropTypes.string,
 		renderShape:React.PropTypes.string,
 		renderSize:React.PropTypes.number,
-		pressDrag: React.PropTypes.func,
+		offsetX:React.PropTypes.number,
+		offsetY:React.PropTypes.number,
+		renderCorlor:React.PropTypes.string,
+		reverse:React.PropTypes.bool,
+		pressDrag:React.PropTypes.func
 	};
+	componentWillMount() {
+		if(this.reverse == false)
+			this.state.pan.addListener((c) => this.state._value = c);
+	}
+	componentWillUnmount() {
+		this.state.pan.removeAllListeners();
+	}
 	constructor(props) {
 		super(props);
-		const { pressDrag, renderText, renderSize } = props;
+		const { pressDrag, renderText, renderShape, renderSize, renderColor, offsetX, offsetY, reverse} = props;
 		this.pressDrag = pressDrag;
-		this.renderText = renderText ? renderText : '+';
+		this.renderShape = renderShape ? renderShape : 'circle';
+		this.renderColor = renderColor ? renderColor : 'yellowgreen';
+		this.renderText = renderText ? renderText : '＋';
 		this.renderSize = renderSize ? renderSize : 36;
+		this.offsetX = offsetX!=null ? offsetX : 100;
+		this.offsetY = offsetY!=null ? offsetY : 100;
+		this.reverse = reverse!=null ? reverse : true;
 		this.state = {
-			pan:new Animated.ValueXY()   
+			pan:new Animated.ValueXY(), 
+			_value:{x: 0, y: 0}
 		};
 
 		this.panResponder = PanResponder.create({    
 			onMoveShouldSetPanResponder: (evt, gestureState) => true,
 			onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+			onPanResponderGrant: (e, gestureState) => {
+				if(this.reverse == false) {
+					this.state.pan.setOffset({x: this.state._value.x, y: this.state._value.y});
+					this.state.pan.setValue({x: 0, y: 0});
+				}
+			},
 			onPanResponderMove: Animated.event([null,{ 
 				dx:this.state.pan.x,
 				dy:this.state.pan.y
 			}]),
-			onPanResponderRelease: (e, gesture) => {
-				Animated.spring(            
-					this.state.pan,         
-					{toValue:{x:0,y:0}}     
-				).start();
+			onPanResponderRelease: (e, gestureState) => {
+				if(this.reverse == false)
+					this.state.pan.flattenOffset();
+				else {
+					Animated.spring(            
+						this.state.pan,         
+						{toValue:{x:0,y:0}}     
+					).start();
+				}
 			} 
 		});
 	}
-	_positionCss = (size)=>{
+	_positionCss = (size,x,y)=>{
+		console.log(x,y);
 		let Window = Dimensions.get('window');
 		return {
+			zIndex:999,
 			position: 'absolute',
-			top: Window.height/2 - size+100,
-			left: Window.width/2 - size+100,
+			top: Window.height/2 - size+y,
+			left: Window.width/2 - size+x,
+
 		};
 	}
-	_dragItemCss = (size)=>{
-		return{
-			backgroundColor: '#1abc9c',
-			width: size*2,
-			height: size*2,
-			borderRadius: size 
-		};
+	_dragItemCss = (size,color,shape)=>{
+		if(shape == 'circle'){
+			return{
+				backgroundColor: color,
+				width: size*2,
+				height: size*2,
+				borderRadius: size 
+			};
+		}else if(shape == 'square'){
+			return{
+				backgroundColor: color,
+				width: size*2,
+				height: size*2,
+				borderRadius: 0 
+			}
+		}
 	}
 	_dragItemTextCss = (size)=>{
 		return {
@@ -75,12 +113,12 @@ export default class Draggable extends Component {
 	}
 	render() {
 		return (
-			<View style={this._positionCss(this.renderSize)}>
+			<View style={this._positionCss(this.renderSize,this.offsetX,this.offsetY)}>
 				<Animated.View 
 					{...this.panResponder.panHandlers}
 					style={[this.state.pan.getLayout(), this._dragItemCss(this.renderSize)]}>
 					<TouchableOpacity 
-						style={this._dragItemCss(this.renderSize)}
+						style={this._dragItemCss(this.renderSize,this.renderColor,this.renderShape)}
 						onPress={this.pressDrag}>
 					<Text style={this._dragItemTextCss(this.renderSize)}>{this.renderText}</Text>
 					</TouchableOpacity>
