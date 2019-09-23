74e9366da0c0c56e49d13b85d538fae3f30afac6
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-09-23 10:16:00 +0530

    Added changes suggested by admin

--- Draggable.js
+++ Draggable.js
@@ -20,42 +20,42 @@ import PropTypes from 'prop-types';
 
 export default class Draggable extends Component {
 	static propTypes = {
-		renderText:PropTypes.string,
-		renderShape:PropTypes.string,
-		renderSize:PropTypes.number,
-		imageSource:PropTypes.oneOfType([
+		renderText: PropTypes.string,
+		renderShape: PropTypes.string,
+		renderSize: PropTypes.number,
+		imageSource: PropTypes.oneOfType([
 			PropTypes.shape({
 				uri: PropTypes.string,
 			}),
 			PropTypes.number
-		]), 
-		offsetX:PropTypes.number,
-		offsetY:PropTypes.number,
-		renderColor:PropTypes.string,
-		reverse:PropTypes.bool,
-		pressDrag:PropTypes.func,
-		onMove:PropTypes.func,
-		pressDragRelease:PropTypes.func,
-		longPressDrag:PropTypes.func,
-		pressInDrag:PropTypes.func,
-		pressOutDrag:PropTypes.func,
-		z:PropTypes.number,
-		x:PropTypes.number,
-		y:PropTypes.number
-		
+		]),
+		offsetX: PropTypes.number,
+		offsetY: PropTypes.number,
+		renderColor: PropTypes.string,
+		reverse: PropTypes.bool,
+		pressDrag: PropTypes.func,
+		onMove: PropTypes.func,
+		pressDragRelease: PropTypes.func,
+		longPressDrag: PropTypes.func,
+		pressInDrag: PropTypes.func,
+		pressOutDrag: PropTypes.func,
+		z: PropTypes.number,
+		x: PropTypes.number,
+		y: PropTypes.number
+
 	};
 	static defaultProps = {
-		offsetX : 100,
-		renderShape : 'circle',
-		renderColor : 'yellowgreen',
-		renderText : '＋',
-		renderSize : 36,
-		offsetY : 100,
-		reverse : true
+		offsetX: 100,
+		renderShape: 'circle',
+		renderColor: 'yellowgreen',
+		renderText: '＋',
+		renderSize: 36,
+		offsetY: 100,
+		reverse: true
 	}
 
 	componentWillMount() {
-		if(this.props.reverse == false)
+		if (this.props.reverse == false)
 			this.state.pan.addListener((c) => this.state._value = c);
 	}
 	componentWillUnmount() {
@@ -65,37 +65,37 @@ export default class Draggable extends Component {
 		super(props, defaultProps);
 		const { pressDragRelease, reverse, onMove } = props;
 		this.state = {
-			pan:new Animated.ValueXY(), 
-			_value:{
-				x: 0, 
+			pan: new Animated.ValueXY(),
+			_value: {
+				x: 0,
 				y: 0
 			}
 		};
 
-		this.panResponder = PanResponder.create({		
+		this.panResponder = PanResponder.create({
 			onMoveShouldSetPanResponder: (evt, gestureState) => true,
 			onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
 			onPanResponderGrant: (e, gestureState) => {
-				if(reverse == false) {
-					this.state.pan.setOffset({x: this.state._value.x, y: this.state._value.y});
-					this.state.pan.setValue({x: 0, y: 0});
+				if (reverse == false) {
+					this.state.pan.setOffset({ x: this.state._value.x, y: this.state._value.y });
+					this.state.pan.setValue({ x: 0, y: 0 });
 				}
 			},
-			onPanResponderMove: Animated.event([null,{ 
-				dx:this.state.pan.x,
-				dy:this.state.pan.y
-			}], {listener: onMove}),
+			onPanResponderMove: Animated.event([null, {
+				dx: this.state.pan.x,
+				dy: this.state.pan.y
+			}], { listener: onMove }),
 			onPanResponderRelease: (e, gestureState) => {
-				if(pressDragRelease)
+				if (pressDragRelease)
 					pressDragRelease(e, gestureState);
-				if(reverse == false)
+				if (reverse == false)
 					this.state.pan.flattenOffset();
-				else 
+				else
 					this.reversePosition();
-			} 
+			}
 		});
 	}
-	
+
 	_positionCss = () => {
 		let Window = Dimensions.get('window');
 		const { renderSize, offsetX, offsetY, x, y, z } = this.props;
@@ -104,45 +104,45 @@ export default class Draggable extends Component {
 				zIndex: z != null ? z : 999,
 				position: 'absolute',
 				top: y != null ? y : (Window.height / 2 - renderSize + offsetY),
-				left: x !=null ? x : (Window.width / 2 - renderSize + offsetX)
+				left: x != null ? x : (Window.width / 2 - renderSize + offsetX)
 			},
 			android: {
 				position: 'absolute',
-				width:Window.width,
-				height:Window.height,
+				width: Window.width,
+				height: Window.height,
 				top: y != null ? y : (Window.height / 2 - renderSize + offsetY),
-				left: x !=null ? x : (Window.width / 2 - renderSize + offsetX)
+				left: x != null ? x : (Window.width / 2 - renderSize + offsetX)
 			},
 		});
 	}
 
 	_dragItemCss = () => {
 		const { renderShape, renderSize, renderColor } = this.props;
-		if(renderShape == 'circle') {
-			return{
+		if (renderShape == 'circle') {
+			return {
 				backgroundColor: renderColor,
 				width: renderSize * 2,
 				height: renderSize * 2,
-				borderRadius: renderSize 
+				borderRadius: renderSize
 			};
-		}else if(renderShape == 'square') {
-			return{
+		} else if (renderShape == 'square') {
+			return {
 				backgroundColor: renderColor,
 				width: renderSize * 2,
 				height: renderSize * 2,
-				borderRadius: 0 
+				borderRadius: 0
 			};
-		}else if(renderShape == 'image') {
-			return{
+		} else if (renderShape == 'image') {
+			return {
 				width: renderSize,
-				height: renderSize 
+				height: renderSize
 			};
 		}
 	}
 	_dragItemTextCss = () => {
 		const { renderSize } = this.props;
 		return {
-			marginTop: renderSize-10,
+			marginTop: renderSize - 10,
 			marginLeft: 5,
 			marginRight: 5,
 			textAlign: 'center',
@@ -151,30 +151,27 @@ export default class Draggable extends Component {
 	}
 	_getTextOrImage = () => {
 		const { renderSize, renderShape, renderText, imageSource, children } = this.props;
-		if(renderShape == 'image') {
-			return(<Image style={this._dragItemCss(renderSize, null, 'image')} source={imageSource}/>);
-		}else{
-			if(children===null)
-				return (<Text style={this._dragItemTextCss(renderSize)}>{renderText}</Text>);
-			else
-				return (children)
-		}
-
+		if (renderShape == 'image')
+			return (<Image style={this._dragItemCss(renderSize, null, 'image')} source={imageSource} />);
+		else if (children === null)
+			return (<Text style={this._dragItemTextCss(renderSize)}>{renderText}</Text>);
+		else
+			return (children);
 	}
 
 	reversePosition = () => {
-		Animated.spring(						
-			this.state.pan,				 
-			{toValue:{x:0,y:0}}		 
+		Animated.spring(
+			this.state.pan,
+			{ toValue: { x: 0, y: 0 } }
 		).start();
 	}
 
 	getPosition = () => {
-		return { 
+		return {
 			offsetX: this.state._value.x,
 			offsetY: this.state._value.y,
 			x: this.state._value.x + this.props.x,
-			y: this.state._value.y + this.props.y 
+			y: this.state._value.y + this.props.y
 		};
 	}
 
@@ -184,17 +181,17 @@ export default class Draggable extends Component {
 
 		return (
 			<View style={this._positionCss()}>
-				<Animated.View 
+				<Animated.View
 					{...this.panResponder.panHandlers}
 					style={[this.state.pan.getLayout()]}>
-					<TouchableOpacity 
+					<TouchableOpacity
 						style={this._dragItemCss()}
 						onPress={pressDrag}
 						onLongPress={longPressDrag}
 						onPressIn={pressInDrag}
 						onPressOut={pressOutDrag}
 					>
-						{touchableContent}	
+						{touchableContent}
 					</TouchableOpacity>
 				</Animated.View>
 			</View>
